f878f50088670764b45262a92123840f5fd79853
Mahdi Ali-Raihan <asder8215@gmail.com>
2022-08-27 19:41:22 -0400

    Working on Navbar

---
+++ personal-website/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Navbar from './Navbar';
+
+// class App extends React.Component {
+//   render () {
+//     return (
+//       <div className='App'> 
+//           <h1> Hello there!</h1>
+//       </div>
+//     )
+//   }
+// }
+ReactDOM.render(<Navbar />, document.getElementById('root'))
