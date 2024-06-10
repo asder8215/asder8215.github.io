88726616e9d921eabf547858321a18f9a3a1f9dd
Mahdi Ali-Raihan <asder8215@gmail.com>
2024-06-10 00:44:08 -0400

    terminal and about me section split appropriately and has styling

--- personal-website/index.js
+++ personal-website/index.js
@@ -1,9 +1,10 @@
-$('body').terminal({
+$('.terminalSection').terminal({
     cat: function(width = 200, height = 300) {
         return $(`<img src="https://placekitten.com/${width}/${height}">`);
     },
-    title: function() {
-        return fetch('https://terminal.jcubic.pl')
+    title: function(...args) {
+        const options = $.terminal.parse_options(args);
+        return fetch(options.url || 'https://terminal.jcubic.pl')
             .then(r => r.text())
             .then(html => html.match(/<title>([^>]+)<\/title>/)[1]);
     }
