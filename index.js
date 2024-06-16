46ede12a0e0cf1fe8e54a37efc203e50a807d65a
Mahdi Ali-Raihan <asder8215@gmail.com>
2024-06-16 13:43:07 -0400

    ls command implemented in terminal index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 let currPath = '~';
-// const rootFileNode = FileNode(currPath, 'dir');
-// const projNode = FileNode("projects", 'dir')
+const rootFileNode = new FileNode(currPath, "dir", "Placeholder");
+const projNode = new FileNode("projects", 'dir', "Placeholder", rootFileNode);
+const expNode = new FileNode("experiences", 'dir', "Placeholder", rootFileNode);
+const eduNode = new FileNode("education", 'dir', "Placeholder", rootFileNode);
+const devNode = new FileNode("leadership", 'dir', "Placeholder", rootFileNode);
+const miscNode = new FileNode("miscellaneous", 'dir', "Placeholder", rootFileNode);
+
+let currNode = rootFileNode;
 
 $('.terminalSection').terminal({
     cat: function(width = 200, height = 300) {
@@ -14,6 +20,20 @@ $('.terminalSection').terminal({
     },
     test: function(){
         // this.set_prompt("hello"); set_prompt is how you denote the terminal's current path
+    },
+    ls: function(){
+        var childrenList = currNode.getChildren();
+        // console.log(currNode.getFileName());
+        // console.log(currNode.getChildren());
+        for(let i = 0; i < childrenList.length; i++){
+            // console.log(childrenList[i].getFileName());
+            if (childrenList[i].getType() === 'dir'){
+                this.echo('[[;blue;]' + childrenList[i].getFileName() + ']');
+            }
+            else{
+                this.echo(childrenList[i].getFileName());
+            }
+        }
     }
 }, {
     checkArity: false,
